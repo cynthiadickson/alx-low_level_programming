376330870bad89e6290359485b27eabedee531ba
cynthiadickson <cynthiadickson3@gmail.com>
2023-02-16 18:59:08 +0000

    101-quote.c resolved

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * main - prints a line using write function
@@ -7,6 +6,7 @@
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59
+);
 	return (1);
+}
