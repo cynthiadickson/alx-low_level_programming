661ab94c5779613f36f0b38926690250192d4844
cynthiadickson <cynthiadickson3@gmail.com>
2023-02-23 13:07:15 +0000

    Task 3-print_remaining_days resolved

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,30 +12,27 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || year % 100 == 0) && !(year % 400 == 0))
+	if ((year % 100 == 0 &&  year % 400 == 0) || (year % 4 == 0))
 	{
-		int leap_days = 366;
 
-		if (day >= 60 && month > 2)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
 
 		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", leap_days - day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
-		if (day == 60 && month == 2)
+		if (month == 2 && day ==  60)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
-			int standard_days = 365;
-
 			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", standard_days - day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 }
