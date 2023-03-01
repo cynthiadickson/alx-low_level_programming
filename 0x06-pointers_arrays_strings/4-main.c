ecac9b7caabc3c8aed0320cb8abe722ba4450dde
cynthiadickson <cynthiadickson3@gmail.com>
2023-03-01 16:06:02 +0000

    Adds a function that reverses the content of an array of integers

--- 0x06-pointers_arrays_strings/4-main.c
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -8,7 +8,7 @@
  *
  * Return: nothing.
  */
-void print_array(int *a, int n);
+void print_array(int *a, int n)
 {
 	int i;
 
@@ -30,11 +30,11 @@ void print_array(int *a, int n);
  *
  * Return: Always 0.
  */
-int main(void);
+int main(void)
 {
 	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337,};
 
-	print array(a, sizeof(a) / sizeof(int));
+	print_array(a, sizeof(a) / sizeof(int));
 	reverse_array(a, sizeof(a) / sizeof(int));
 	print_array(a, sizeof(a) / sizeof(int));
 	return (0);
